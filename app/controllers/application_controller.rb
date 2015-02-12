e1310d99707586d678a12d9c678c50cd3bfbfa4f
BuckSpencer <ad3uck@gmail.com>
2015-02-12 13:15:41 -0600

    devise fully integrated for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 
   protected
+
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :profile_name
